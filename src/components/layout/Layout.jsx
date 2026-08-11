afa098072c13aae39ffd0773cbd3b758312efca5
kemiiiii01 <rm93910@estudante.fieb.edu.br>
2026-08-11 10:47:41 -0300

    ajustess

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,13 @@
+
 import { Outlet, useLocation } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./Footer";
 
 export default function Layout() {
-
   const location = useLocation();
 
   const isTermos = location.pathname === "/termos-de-uso";
-  const isPoliticaDePrivacidade = location.pathname === "/politica-de-privacidade";
+  const isPolitica = location.pathname === "/politica-de-privacidade";
 
   return (
     <>
@@ -15,10 +15,10 @@ export default function Layout() {
 
       <Outlet />
 
-      <Footer 
-        waveColor={isTermos ? "#f5f5f5" : "#ffffff"}
-        waveColor={isPoliticaDePrivacidade ? "#f5f5f5" : "#ffffff"}
+      <Footer
+        waveColor={isTermos || isPolitica  ? "#f5f5f5" : "#ffffff"}
       />
     </>
   );
 }
+
